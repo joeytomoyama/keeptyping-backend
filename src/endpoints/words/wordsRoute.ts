feda842fec158ab96105fd05dbb2ea734e1d642f
Joey Prüssing <j.pruessing28@gmail.com>
2023-10-18 03:59:50 +0200

    connection to postgres db successful.

--- src/endpoints/words/wordsRoute.ts
+++ src/endpoints/words/wordsRoute.ts
@@ -1,9 +1,24 @@
-import express, { Request, Response } from 'express'
+import express, { Express, Request, Response } from 'express'
 
-const app = express()
+import { getUsers, englishWords, germanWords } from './wordsService'
+
+const app: Express = express()
 
 app.get('/', (request: Request, response: Response) => {
   return response.json({ message: 'Hello World!' })
+})
+
+app.get('/english', (request: Request, response: Response) => {
+    return response.json({ message: englishWords })
+})
+
+app.get('/german', (request: Request, response: Response) => {
+    return response.json({ message: germanWords })
+})
+
+app.get('/test', async (request: Request, response: Response) => {
+    console.log(await getUsers())
+    return response.json({ message: 'Hello Test!' })
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,13 @@ import dotenv from 'dotenv'
 
 import wordsRoute from './endpoints/words/wordsRoute'
 
+// SETUP
 dotenv.config()
-
 const app = express()
-
 app.use(express.json())
 
-
+// ENDPOINTS
 app.use('/api/words', wordsRoute)
 
 
-app.listen(3333, () => console.log('Server is running!'))
+app.listen(process.env.PORT, () => console.log(`Server is running at port: ${process.env.PORT}!`))
